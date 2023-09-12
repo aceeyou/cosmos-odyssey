a7745537a238df4e630ab3180c6815211304459f
Ace Logronio <64186285+aceeyou@users.noreply.github.com>
2023-09-12 23:25:36 +0800

    first init

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./navbar.css";
+
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <div className="nav__container">
+      <img className="nav__logo" src="logo.svg" alt="odessy logo" />
+      <nav className="nav">
+        <ul className="nav__list">
+          <li>Stay Connected</li>
+          <li>Education</li>
+          <li>Community</li>
+          <li>About Us</li>
+        </ul>
+        <button className="nav__sign-in-btn">Sign in</button>
+      </nav>
+      {isMenuOpen && (
+        <nav className="nav__mobile">
+          <ul>
+            <li>Stay Connected</li>
+            <li>Education</li>
+            <li>Community</li>
+            <li>About Us</li>
+          </ul>
+          <button className="nav__sign-in-btn">Sign in</button>
+        </nav>
+      )}
+      <button
+        className="nav__menu-btn"
+        onClick={() => setIsMenuOpen((isOpen) => !isOpen)}
+      >
+        <img src="/icons/menu.svg" alt="mobile menu button" />
+      </button>
+    </div>
+  );
+}

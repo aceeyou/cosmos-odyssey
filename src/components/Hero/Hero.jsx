6269216d5059d606f15b0882c173957971235b80
Ace Logronio <64186285+aceeyou@users.noreply.github.com>
2023-09-18 15:55:58 +0800

    fixed mobile layout styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,102 +3,94 @@ import "./hero.css";
 export default function Hero() {
   return (
     <div className="hero">
-      <div className="img-container">
-        <img
-          className="hero-img-earth"
-          src={process.env.PUBLIC_URL + "/images/earth.jpg"}
-          alt="hero banner earth"
-        />
+      <img
+        src={process.env.PUBLIC_URL + "/images/earth.jpg"}
+        alt="earth"
+        className="hero-earth"
+      />
+      <div className="hero-headline">
+        <h1>Unveiling the Wonders of the Universe</h1>
       </div>
-      <h2 className="hero__headline">Unveiling the Wonders of the Universe</h2>
-      <div className="hero__astronaut-container">
-        <img
-          src={process.env.PUBLIC_URL + "/images/astronaut.png"}
-          alt="astronaut hero"
-          className="astronaut-img"
-        />
-        <div className="hero__astronaut-shadow"></div>
-        {/* TOTAL TIME IN SPACE */}
-        <div className="total-time-in-space-container">
-          <div className="hero__astronaut-boxes-top top">
-            <div className="icon-container time-icon">
+      <Astronaut />
+      <div className="hero-description">
+        <p>
+          At odissey, our mission is to inspire, educate, and ignite your
+          passion for the cosmos. We are your premier destination for all things
+          related to space exploration, astronomy, and the mysteries of the
+          universe. Whether you're a seasoned astronomer or a curious novice,
+          join us on a journey to the furthest reaches of the cosmos.{" "}
+        </p>
+        <div className="buttons">
+          <button className="hero-get-startedd">Get Started</button>
+          <label htmlFor="watch-video">
+            Watch Video
+            <div className="icon-container play-icon-container">
               <img
-                src={process.env.PUBLIC_URL + "/icons/stopwatch.svg"}
-                alt="stopwatch icon"
+                src={process.env.PUBLIC_URL + "/icons/play.svg"}
+                alt="play video"
               />
             </div>
-            <p>Total time in space</p>
-          </div>
-          <div className="hero__astronaut-boxes-bottom bottom">
-            <p className="time-in-space-text">352 D 5H 49 M 20 S</p>
-          </div>
-        </div>
-        {/* SPACEWALKS */}
-        <div className="spacewalks-container">
-          <div className="hero__astronaut-boxes-top top">
-            <div className="icon-container spacewalks-icon">
-              <img
-                src={process.env.PUBLIC_URL + "/icons/chart-pie.svg"}
-                alt="spacewalks icon"
-              />
-            </div>
-            <p>Spacewalks</p>
-          </div>
-          <div className="hero__astronaut-boxes-bottom bottom">
-            <img
-              src={process.env.PUBLIC_URL + "/images/line-graph.png"}
-              alt="spacewalks line graph"
-            />
-            <img
-              src={process.env.PUBLIC_URL + "/images/pie.png"}
-              alt="spacewalks pie graph"
-            />
-          </div>
-        </div>
-        {/* EXERCISING */}
-        <div className="exercising-container">
-          <div className="hero__astronaut-boxes-top top">
-            <div className="icon-container exercising-icon">
-              <img
-                src={process.env.PUBLIC_URL + "/icons/heart.svg"}
-                alt="exercising icon"
-              />
-            </div>
-            <p>Exercising</p>
-          </div>
-          <div className="hero__astronaut-boxes-bottom bottom">
-            <img
-              src={process.env.PUBLIC_URL + "/images/pulse.png"}
-              alt="spacewalks line graph"
-            />
-          </div>
+          </label>
         </div>
       </div>
-      {/* HERO DESCRIPTION */}
-      <section className="hero__description">
-        <article>
-          <h1 className="sr-only">ODYSSEY</h1>
-          <h3 className="sub-heading">
-            At odissey, our mission is to inspire, educate, and ignite your
-            passion for the cosmos. We are your premier destination for all
-            things related to space exploration, astronomy, and the mysteries of
-            the universe. Whether you're a seasoned astronomer or a curious
-            novice, join us on a journey to the furthest reaches of the cosmos.{" "}
-          </h3>
-          <div className="hero__desctiption-buttons">
-            <button className="get-started">Get Started</button>
-            <div className="watch-video-container">
-              <p>Watch Video</p>
-              <div className="icon-container watch-video-btn watch-video-icon">
-                <img
-                  src={process.env.PUBLIC_URL + "/icons/play.svg"}
-                  alt="watch video icon"
-                />
-              </div>
-            </div>
+    </div>
+  );
+}
+
+function Astronaut() {
+  return (
+    <div className="astronaut-group">
+      <img
+        src={process.env.PUBLIC_URL + "/images/astronaut.png"}
+        alt="astronaut"
+        className="astronaut-hero-img"
+      />
+      {/* time in space */}
+      <div className="time-in-space-box">
+        <div className="top">
+          <div className="icon-container time-icon-container">
+            <img
+              src={process.env.PUBLIC_URL + "/icons/stopwatch.svg"}
+              alt="stopwatch"
+            />
           </div>
-        </article>
-      </section>
+          <p>Total time in space</p>
+        </div>
+        <div className="bottom">
+          <p>352 D 5 H 49 M 20 S</p>
+        </div>
+      </div>
+      {/* spacewalks */}
+      <div className="box spacewalks-box">
+        <div className="top">
+          <div className="icon-container icon-spacewalks">
+            <img src={process.env.PUBLIC_URL + "/icons/chart-pie.svg"} alt="" />
+          </div>
+          <p>Spacewalks</p>
+        </div>
+        <div className="bottom">
+          <img
+            src={process.env.PUBLIC_URL + "/images/line-graph.png"}
+            alt="line graph"
+          />
+          <img src={process.env.PUBLIC_URL + "/images/pie.png"} alt="pie" />
+        </div>
+      </div>
+      {/* exercising */}
+      <div className="box exercising-box">
+        <div className="top">
+          <div className="icon-container icon-spacewalks">
+            <img src={process.env.PUBLIC_URL + "/icons/chart-pie.svg"} alt="" />
+          </div>
+          <p>Exercising</p>
+        </div>
+        <div className="bottom">
+          <img
+            src={process.env.PUBLIC_URL + "/images/pulse.png"}
+            alt="line graph"
+          />
+        </div>
+      </div>
     </div>
   );
 }
